691ba4ac08d76d7d01e9b245658975cf18073b5e
Divy Deshmukh <divydeshmukh1104@gmail.com>
2026-02-26 13:32:39 +0530

    added nextjs in web folder

---
+++ web/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Interview Shield AI",
+  description: "Real-time AI that analyzes video/audio during remote interviews to detect suspicious behavior and support secure hiring.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
